dd11cfb445134c23ee9dfbb7fb029b35d1ca25a3
Driner Tucto <1304745@senati.pe>
2022-11-04 12:56:24 -0500

    enviar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -39,9 +41,7 @@
                 color: red;
             }
 
-            }
-           
-         
+             
         </style>
     </head>
     <body class="antialiased">
