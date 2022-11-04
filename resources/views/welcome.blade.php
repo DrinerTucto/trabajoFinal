d387b56ffad6ce7f7fbcdbb873356271f859ec9c
Driner Tucto <1304745@senati.pe>
2022-11-04 15:04:01 -0500

    corregido

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,10 +22,6 @@
                 color: black;
                 margin: 0 auto;
                 font-size: 50px;
-<<<<<<< HEAD
-=======
-
->>>>>>> fc66b62ba87a356fe7a827340c6cb39ad2334fb0
             }
             span {
                     color: red;
@@ -35,19 +31,11 @@
                 color: blues;
                 border-radius: 21%;
                 font-family:Bodoni MT;
-<<<<<<< HEAD
-=======
-
->>>>>>> fc66b62ba87a356fe7a827340c6cb39ad2334fb0
                 
             }
             a:hover{
                 color: red;
             }
-<<<<<<< HEAD
-=======
-
->>>>>>> fc66b62ba87a356fe7a827340c6cb39ad2334fb0
            
          
         </style>
@@ -113,21 +101,11 @@
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                                 <a href="https://accounts.google.com/v3/signin/identifier?dsh=S-787652449%3A1667582050555009&hl=es&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=ARgdvAua-CiUM2_rghgd-XxTpiCOBXqKPx2t1A5t23T6o3unwr20zzMxMzEL8tHsDuzfVRg6nlnNOA"target=block><div>
-<<<<<<< HEAD
                             <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Contactame <span>👉Aqui</span></div>
-=======
-                            <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Contactame <span>Aqui</span></div>
->>>>>>> fc66b62ba87a356fe7a827340c6cb39ad2334fb0
                             </div>
 
                                 </div> </a>
 
-<<<<<<< HEAD
-=======
-
-                            
-
->>>>>>> fc66b62ba87a356fe7a827340c6cb39ad2334fb0
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                     Laravel's robust library of first-party tools and libraries, such as <a href="https://forge.laravel.com" class="underline">Forge</a>, <a href="https://vapor.laravel.com" class="underline">Vapor</a>, <a href="https://nova.laravel.com" class="underline">Nova</a>, and <a href="https://envoyer.io" class="underline">Envoyer</a> help you take your projects to the next level. Pair them with powerful open source libraries like <a href="https://laravel.com/docs/billing" class="underline">Cashier</a>, <a href="https://laravel.com/docs/dusk" class="underline">Dusk</a>, <a href="https://laravel.com/docs/broadcasting" class="underline">Echo</a>, <a href="https://laravel.com/docs/horizon" class="underline">Horizon</a>, <a href="https://laravel.com/docs/sanctum" class="underline">Sanctum</a>, <a href="https://laravel.com/docs/telescope" class="underline">Telescope</a>, and more.
