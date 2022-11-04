1114e3e41db66fd1eb7752829500262c044f30ac
Driner Tucto <1304745@senati.pe>
2022-11-04 14:51:16 -0500

    hola

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -24,7 +22,6 @@
                 color: black;
                 margin: 0 auto;
                 font-size: 50px;
-
             }
             span {
                     color: red;
@@ -34,14 +31,13 @@
                 color: blues;
                 border-radius: 21%;
                 font-family:Bodoni MT;
-
                 
             }
             a:hover{
                 color: red;
             }
-
-             
+           
+         
         </style>
     </head>
     <body class="antialiased">
@@ -57,7 +53,7 @@
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                 <a href="https://github.com/DrinerTucto/trabajoFinal/blob/main/resources/views/welcome.blade.php" target="block">
-                <h1 class="driner"><span>D</span>riner <span>T</span>ucto  <span>N</span>ieto</h1>
+                <h1 class="driner"><span>⭐D</span>riner <span>T</span>ucto  <span>N</span>ieto⭐</h1>
                 </a>
                 </div>
                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
@@ -105,13 +101,10 @@
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                                 <a href="https://accounts.google.com/v3/signin/identifier?dsh=S-787652449%3A1667582050555009&hl=es&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=ARgdvAua-CiUM2_rghgd-XxTpiCOBXqKPx2t1A5t23T6o3unwr20zzMxMzEL8tHsDuzfVRg6nlnNOA"target=block><div>
-                            <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Contactame <span>Aqui</span></div>
+                            <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Contactame <span>👉Aqui</span></div>
                             </div>
 
                                 </div> </a>
-
-
-                            
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
@@ -150,4 +143,4 @@
             </div>
         </div>
     </body>
-</html>
+</html>s
